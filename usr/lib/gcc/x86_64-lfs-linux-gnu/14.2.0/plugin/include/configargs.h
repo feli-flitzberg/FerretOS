d1c88d84799be4500d9f75f209b5fe64c0732d64
feli-flitzberg <feliflitzberg@proton.me>
2024-11-14 12:17:21 -0700

    finish chapter 6

---
+++ usr/lib/gcc/x86_64-lfs-linux-gnu/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --build= --host=x86_64-lfs-linux-gnu --target=x86_64-lfs-linux-gnu LDFLAGS_FOR_TARGET=-L/opt/lfs/sources/gcc-14.2.0/build/x86_64-lfs-linux-gnu/libgcc --prefix=/usr --with-build-sysroot=/opt/lfs --enable-default-pie --enable-default-ssp --disable-nls --disable-multilib --disable-libatomic --disable-libgomp --disable-libquadmath --disable-libsanitizer --disable-libssp --disable-libvtv --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
